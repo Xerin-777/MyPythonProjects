234fd9eb67a2841cd771f3e40573a3f35432d34a
Xerin-777 <patrykglanc32@gmail.com>
2025-08-07 15:48:38 +0200

    Dodano projekt cwiczeniowy z funkcjami

---
+++ funkcje_cwiczenie/main.py
@@ -0,0 +1,8 @@
+def oblicz_pole_prostokata(dlugosc, szerokosc):
+    pole = dlugosc * szerokosc
+    print(f"Pole prostokąta o długości {dlugosc} i szerokości {szerokosc} wynosi: {pole}")
+
+
+# Wywołanie funkcji z różnymi wartościami
+oblicz_pole_prostokata(5,10)
+oblicz_pole_prostokata(8,7)
